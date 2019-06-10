dd9c046376263b8c7b9beb0ef69681e3c8ad66a4
Mohammed Basith <extmbasith@fleetcor.com>
2019-06-10 19:32:00 +0530

    1.0

---
+++ src/com/test/Test.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Temp\\JavaCABuild\\Drivers\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://extmbasith:Basila@03@czprgtcbpt.efn.ccs.cz/gfnlaunch/");
+		driver.findElement(By.id("MainContent_grdServers_imgContent_0")).click();
+	}
+
+}
