c0cb90261503a49d85338765ffd56fdd4ae56453
Mohammed Basith <extmbasith@fleetcor.com>
2019-06-13 13:51:40 +0530

    2.2

--- src/comtest/Testclass.java
+++ src/comtest/Testclass.java
@@ -4,21 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Test1 {
+public class Testclass {
 	
 	
 	public static void main(String[] args) {
 		
 		
-		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\eclipse\\Test\\driver\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://extmbasith:Basila@03@czprgtcbpt.efn.ccs.cz/gfnlaunch/");
-		driver.findElement(By.id("MainContent_grdServers_imgContent_0ooooo")).click();
+		driver.findElement(By.id("MainContent_grdServers_imgContent_0")).click();
 		
-		System.out.println("Passvsdbvvn");
+		System.out.println("passed");
 		
-		System.out.println("Passvsdbvvn");
+	
 	}
 
 }
+	
